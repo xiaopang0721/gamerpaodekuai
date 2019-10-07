9b4b162bf70619fe446a523cb21e376056557211
happy <happy>
2019-10-07 15:10:53 +0800

    祈福预加载

--- page/RpaodekuaiPageDef.ts
+++ page/RpaodekuaiPageDef.ts
@@ -23,7 +23,7 @@ module gamerpaodekuai.page {
 				PathGameTongyong.atlas_game_ui_tongyong + "touxiang.atlas",
 				PathGameTongyong.atlas_game_ui_tongyong + "pai.atlas",
 				PathGameTongyong.atlas_game_ui_tongyong + "fk.atlas",
-				PathGameTongyong.atlas_game_ui_tongyong + "qifu.atlas",
+				DatingPath.atlas_dating_ui + "qifu.atlas",
 				PathGameTongyong.atlas_game_ui_tongyong + "chongzhi.atlas",
 				PathGameTongyong.atlas_game_ui_tongyong + "general/effect/fapai_1.atlas",
 				PathGameTongyong.atlas_game_ui_tongyong + "general/effect/xipai.atlas",
