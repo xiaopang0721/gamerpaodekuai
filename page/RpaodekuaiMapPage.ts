65f2df7d980c758a40ebd926a679ac839061536d
xiaoer <xiaoer@example.com>
2019-10-18 19:53:22 +0800

    修改 充值按钮增加api判断

--- page/RpaodekuaiMapPage.ts
+++ page/RpaodekuaiMapPage.ts
@@ -120,6 +120,9 @@ module gamerpaodekuai.page {
         // 页面打开时执行函数
         protected onOpen(): void {
             super.onOpen();
+             //api充值不显示
+            this._viewUI.btn_chongzhi.visible = !WebConfig.enterGameLocked;
+            
             this.updateViewUI();
             this.onUpdateUnitOffline();
             if (this._paodekuaiStory instanceof gamecomponent.story.StoryRoomCardBase) {
